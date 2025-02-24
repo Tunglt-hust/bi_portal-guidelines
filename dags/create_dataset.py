e55893bb884c8a25c98702fd73e9a0bffd078192
Tunglt-hust <tunglt.hust.hn@gmail.com>
2025-02-24 11:25:20 +0700

    Update create_dataset.py

--- dags/create_dataset.py
+++ dags/create_dataset.py
@@ -1,26 +1,37 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
+from datetime import timedelta
 
 # Define default arguments
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(1),
-    'retries': 1,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_failure': True,
+    'email_on_retry': False
 }
 
 # Create DAG
-dag = DAG(
+with DAG(
     'create_bigquery_dataset',
     default_args=default_args,
+    description='DAG to execute BigQuery dataset creation script',
+    schedule_interval=None,  # Run manually
     catchup=False,
-)
+    tags=['bigquery']
+) as dag:
 
-# Push data to BigQuery using a bash command
-create_dataset_task = BashOperator(
-    task_id='create_dataset_in_bigquery',
-    bash_command='python /home/airflow/scripts/dataset_creator.py',
-    dag=dag,
-)
+    # Task to execute the Python script
+    execute_script = BashOperator(
+        task_id='create_dataset_in_bigquery',
+        bash_command='python {{ var.value.scripts_path }}/dataset_creator.py',
+        dag=dag
+    )
 
-create_dataset_task
+    # You can add more tasks here if needed
+    
+    # Set task dependencies
+    execute_script
