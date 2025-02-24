aa707d4ed5de6df426a91b441e3b1729f76d4b9d
Tunglt-hust <tunglt.hust.hn@gmail.com>
2025-02-24 11:01:28 +0700

    Update push_data_bigquery.py

--- dags/push_data_bigquery.py
+++ dags/push_data_bigquery.py
@@ -1,1 +1,26 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
 
+# Define default arguments
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'retries': 1,
+}
+
+# Create DAG
+dag = DAG(
+    'push_data_bigquery',
+    default_args=default_args,
+    catchup=False,
+)
+
+# Push data to BigQuery using a bash command
+push_data_task = BashOperator(
+    task_id='push_data_to_bigquery',
+    bash_command='python /home/airflow/scripts/data_ingestion.py',
+    dag=dag,
+)
+
+push_data_task
