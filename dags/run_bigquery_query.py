0512aca95f27d3692c43c733480449a87db72c2c
Tunglt-hust <tunglt.hust.hn@gmail.com>
2025-02-24 11:26:51 +0700

    Update run_bigquery_query.py

--- dags/run_bigquery_query.py
+++ dags/run_bigquery_query.py
@@ -1,26 +1,37 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
+from datetime import timedelta
 
 # Define default arguments
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(1),
-    'retries': 1,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_failure': True,
+    'email_on_retry': False
 }
 
 # Create DAG
-dag = DAG(
+with DAG(
     'run_bigquery_query',
     default_args=default_args,
+    description='DAG to execute BigQuery dataset creation script',
+    schedule_interval=None,  # Run manually
     catchup=False,
-)
+    tags=['bigquery']
+) as dag:
 
-# Push data to BigQuery using a bash command
-run_query_task = BashOperator(
-    task_id='run_query_in_bigquery',
-    bash_command='python /home/airflow/scripts/query_runner.py',
-    dag=dag,
-)
+    # Task to execute the Python script
+    execute_script = BashOperator(
+        task_id='run_saved_query_in_bigquery',
+        bash_command='python {{ var.value.scripts_path }}/data_ingestion.py',
+        dag=dag
+    )
 
-run_query_task
+    # You can add more tasks here if needed
+    
+    # Set task dependencies
+    execute_script
